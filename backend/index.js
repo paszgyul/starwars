717ae9f9bf341361df73c72f2293576855daf05d
Gyula Pasztor <paszgyu@gmail.com>
2023-09-27 15:26:31 +0200

    Add CORS policy

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,12 @@ app.listen(BACKEND_PORT, () => {
 	console.log(`App is listening to port: ${BACKEND_PORT}`);
 });
 
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+	});
+
 app.route("/")
 	.get((req, res) =>{
 		res.send("Hello world")
