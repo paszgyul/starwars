53e7da70440b0eb6272915eec539efc4787e09b7
Gyula Pasztor <paszgyu@gmail.com>
2023-09-27 15:26:54 +0200

    Add React

---
+++ frontend/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container-flex h-100'>
+        <div className='row m-0 p-0 h-100'>
+          <div className='col-12 p-0 home-box'>
+            {/* <img src="home.jpeg" className="w-100" alt="Star Wars" /> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+/*<header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>*/
